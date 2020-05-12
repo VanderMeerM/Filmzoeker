87a61ac3feb4d13071edc93ff3e6df8bfbf8b134
Marcel van der Meer <marcelmeer@gmail.com>
2020-05-12 19:12:43 +0200

    some approvements

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 function addMoviestoDom(movies) {
     const movieList = document.getElementById('moviedatabase').getElementsByTagName('li');
-    const movieWithLi = movies.map(movie => {
+    const movieWithLi = movies.forEach(movie => {
         const newLi = document.createElement("li");
         const newA = document.createElement("a");
         const newImg = document.createElement("img");
         newImg.src = movie.Poster;
-        newA.href = "https://www.imdb.com/title/"+movie.imdbID;
+        newA.href = `https://www.imdb.com/title/${movie.imdbID}`;
         moviedatabase.appendChild(newLi);
         newLi.appendChild(newA);
         newA.appendChild(newImg);
@@ -16,50 +16,53 @@ function addMoviestoDom(movies) {
 
 addMoviestoDom(movies);
 
+function removePreviousMovies() {
+    document.getElementById('nothingfound').innerHTML ='';
+    const movieList = document.getElementById('moviedatabase');
+        let removeMovies = movieList.lastElementChild;
+        while (removeMovies) {
+            movieList.removeChild(removeMovies);
+            removeMovies = movieList.lastElementChild;
+        }}
+
 const recentMovies = document.getElementsByName("moviefilters").
     forEach(filter => {
-        filter.addEventListener("change", function handleOnChangeEvent(event) {
-            const movieList = document.getElementById('moviedatabase');
-            let removeMovies = movieList.lastElementChild;
-            while (removeMovies) {
-                movieList.removeChild(removeMovies);
-                removeMovies = movieList.lastElementChild;
-            }
+        filter.addEventListener("change", 
+            function handleOnChangeEvent(event) {
+                removePreviousMovies();
+               
+               if (event.target.value ==="allmovies") {
+                    addMoviestoDom(movies)} 
+                    
+                else if 
+                 (event.target.value==="recentmovies") {
+                 const filterLatestMovies = movies.filter(movies => movies.Year >= 2014);
+                 addMoviestoDom(filterLatestMovies)
+                 }
+                  
+                else {
+                const title = event.target.value[0].toUpperCase() + event.target.value.slice(1);
+                const filteredMovies = movies.filter(movies => movies.Title.includes(title))
+                addMoviestoDom(filteredMovies);
+                }
 
-            switch (event.target.value) {
-<<<<<<< HEAD
-                case "allmovies":
-=======
-                    case "allmovies":
->>>>>>> edee8ebe2151f69c8e171002c78d28ec58c56d0f
-                   addMoviestoDom(movies);
-                    break;
-                case "recentmovies":
-                    const filterLatestMovies = movies.filter(movies => movies.Year >= 2014);
-                    addMoviestoDom(filterLatestMovies);
-                    break;
-                case "avenger":
-                    const avengerMovies = movies.filter(movies => movies.Title.includes("Avenger"));
-                    addMoviestoDom(avengerMovies);
-                    break;
-                case "xmen":
-                    const xmenMovies = movies.filter(movies => movies.Title.includes("X-Men"));
-                    addMoviestoDom(xmenMovies);
-                    break;
-                case "princess":
-                    const princessMovies = movies.filter(movies => movies.Title.includes("Princess"));
-                    addMoviestoDom(princessMovies);
-                    break;
-                case "batman":
-                    const batmanMovies = movies.filter(movies => movies.Title.includes("Batman"));
-                    addMoviestoDom(batmanMovies);
-                    break;
-                default:
-                    addMoviestoDom(movies)
             }
-        }
         )
     })
+
+function getValue() {
+    if(event.keyCode == 13) {
+        removePreviousMovies();                         
+        const title = event.target.value[0].toUpperCase() + event.target.value.slice(1);
+        const filteredMovies = movies.filter(movies => movies.Title.includes(title))
+        if (filteredMovies.length===0) { 
+            document.getElementById('nothingfound').innerHTML = 'Niets gevonden'} 
+            else {
+        addMoviestoDom(filteredMovies); 
+    }
+    }
+}
+
 
 
 
